cd5fa8eec215a6765662d5ddb68348a572a49829
ife <598978359@qq.com>
2017-03-01 16:14:17 +0800

    2017.3.1 Second

--- js/global.js
+++ js/global.js
@@ -10,29 +10,42 @@ window.onload = function() {
 	}
 	
 	window.scrollTop = 0;
-	
 }
 
+addEventListener("load", function() {
+	setTimeout(function() {
+		window.scrollTo(0,1);
+	}, 0);
+}, false); 
+
 window.onscroll = function() {
-	var scrTop = document.documentElement.scrollTop || document.body.scrollTop; //滚动高度
-	var winH = document.documentElement.clientHeight || document.body.clientHeight; //屏幕高度
+	console.log();
+	var scrTop = window.pagYoffset || document.documentElement.scrollTop || document.body.scrollTop; //滚动高度
+	var winH = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight; //屏幕高度
 //	console.log(scrTop + winH)
 	var eleH = document.documentElement.offsetHeight || document.body.offsetHeight; //元素高度
 //	console.log(eleH);
+	var ani = document.getElementsByClassName('box')[0];
+	if (scrTop + winH == eleH) {
+		setTimeout(add, 1000);
+		ani.style.display = 'block';
+	}
+}
+
+function add() {
 	var wrap = document.getElementsByClassName('scroll')[0];
-	var h = wrap.offsetHeight/2 - 80;
 	var ul = wrap.getElementsByTagName('ul')[0];
 	var num = items.length + 1;
-	if (scrTop + winH == eleH) {
-		for (var i=0; i<20; i++) {
-			var newItem = document.createElement('li');
-			newItem.className = 'scroll-item';
-			ul.appendChild(newItem);
-			var newInner = document.createElement('div');
-			newInner.innerHTML = 'item' + (i + num);
-			newInner.className = 'scroll-inner';
-			newItem.appendChild(newInner);
-			items.push(newItem);
-		}
+	var ani = document.getElementsByClassName('box')[0];
+	for (var i=0; i<20; i++) {
+		var newItem = document.createElement('li');
+		newItem.className = 'scroll-item';
+		ul.appendChild(newItem);
+		var newInner = document.createElement('div');
+		newInner.innerHTML = 'item' + (i + num);
+		newInner.className = 'scroll-inner';
+		newItem.appendChild(newInner);
+		items.push(newItem);
 	}
+	ani.style.display = 'none';
 }
